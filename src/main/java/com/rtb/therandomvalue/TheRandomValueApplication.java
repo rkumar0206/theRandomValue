e90eb37586f607645e18370372b4faf3c25e4a63
Rohit <rkumar8092378845@gmail.com>
2022-03-25 00:02:00 +0530

    - added image urls to all the categories

--- src/main/java/com/rtb/therandomvalue/TheRandomValueApplication.java
+++ src/main/java/com/rtb/therandomvalue/TheRandomValueApplication.java
@@ -33,14 +33,16 @@ public class TheRandomValueApplication {
     @PostConstruct
     private void insertCategories() {
 
-        categoryRepo.save(new Category(NUMBERS_AND_ALPHABETS, ""));
-        categoryRepo.save(new Category(DATES, ""));
-        categoryRepo.save(new Category(FOOD_RECIPES, ""));
-        categoryRepo.save(new Category(IMAGES, ""));
-        categoryRepo.save(new Category(SHOPPING, ""));
-        categoryRepo.save(new Category(GITHUB_REPOSITORIES, ""));
-        categoryRepo.save(new Category(COLORS, ""));
-        categoryRepo.save(new Category(VIDEOS, ""));
-        categoryRepo.save(new Category(ARTICLES, ""));
+        String driveLink = "https://drive.google.com/uc?export=view&id=";
+
+        categoryRepo.save(new Category(NUMBERS_AND_ALPHABETS, driveLink + "1GN7dxDH117IAFSiJCIWpWRJAqYNeGq21"));
+        categoryRepo.save(new Category(DATES, driveLink + "1dBq0kuUeEfuT9RA8gO_yNZ6ioAI8IuRN"));
+        categoryRepo.save(new Category(FOOD_RECIPES, driveLink + "1O52ZpSLO152Ela5kKkmNvlsB60YmDUeq"));
+        categoryRepo.save(new Category(IMAGES, driveLink + "1OWowAmsfLBR6XI-41CnhG0qQOFelPNnI"));
+        categoryRepo.save(new Category(SHOPPING, driveLink + "1xOSTyam8ULvkmNFyHNcCrgB_b-MyZP96"));
+        categoryRepo.save(new Category(GITHUB_REPOSITORIES, driveLink + "1s0Nwxj0kbSQR0crK-rPaisurftpVf1fA"));
+        categoryRepo.save(new Category(COLORS, driveLink + "1ZwTT87jKNqjd841o2uBs0DUlfPNy5vCX"));
+        categoryRepo.save(new Category(VIDEOS, driveLink + "1740Y2jGSeDorFLhbwJdzx1EqF6aEMS8q"));
+        categoryRepo.save(new Category(ARTICLES, driveLink + "166skDkiPSrW2EBr5Ge8urMTm368DRvAt"));
     }
 }
